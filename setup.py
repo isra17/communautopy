38880192670d21b4e5dce65960764c7ef7150ac3
isra17 <isra017@gmail.com>
2018-01-16 21:20:23 -0500

    use setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(name='communautopy',
+                 version='0.1',
+                 description='Communauto Python client library',
+                 long_description=open('README.md').read().strip(),
+                 author='isra17',
+                 author_email='isra017@gmail.com',
+                 url='https://github.com/isra17/communautopy',
+                 py_modules=['communauto'],
+                 install_requires=['zeep'],
+                 license='GNU General Public License v3 (GPLv3)')
+
